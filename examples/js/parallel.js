7c02ee8b9f71170268c966490351f93c3d6369b3
Stone Zhong <stonezhong@hotmail.com>
2016-10-04 22:37:36 -0700

    update examples

--- examples/js/parallel.js
+++ examples/js/parallel.js
@@ -6,31 +6,27 @@ import { run, printf } from './common';
  * 
  */
 
-let task1 = (() => {
-    let i = 0;
-    return SEQ(
-        WHILE(() => i <= 5 ).
-        DO(
-            () => printf('from task 1: i = %d', i),
-            SLEEP(1000),
-            () => { i ++ }
-        ),
-        () => { i = 0; }
-    );
-})();
+let task1 = SEQ(
+    LET("i", 0),                                        // let i = 0;
+    WHILE(({i}) => i <= 5 ).                            // while (i <= 5)            
+    DO(                                                 // {
+        ({i}) => printf('from task 1: i = %d', i),      //     printf('from task 1: i = %d', i); 
+        SLEEP(1000),                                    //     sleep(1000);
+        (local) => { local.i ++ }                       //     i ++;
+    )                                                   // }
+);
 
-let task2 = (() => {
-    let i = 0;
-    return SEQ(
-        WHILE(() => i <= 3 ).
-        DO(
-            () => printf('from task 2: i = %d', i),
-            SLEEP(1500),
-            () => { i ++ }
-        ),
-        () => { i = 0; }
-    );
-})(); 
+
+let task2 = SEQ(
+    LET("i", 0),                                        // let i = 0;
+    WHILE(({i}) => i <= 3 ).                            // while (i <= 3)
+    DO(                                                 // {
+        ({i}) => printf('from task 2: i = %d', i),      //     printf('from task 2: i = %d', i);
+        SLEEP(1500),                                    //     sleep(1500);
+        (local) => { local.i ++ }                       //     i ++;
+    )                                                   // }
+);
+ 
 
 /**
  * This will launch task1 and task2 in parallel, and once both tasks are completed, appMain will complete.
